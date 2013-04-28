5b90276a0f9bcf860fa0979d924748bdf6f34118
Erik Johnson <erjohnson@blackberry.com>
2013-04-28 19:13:21 -0400

    Unit test to ensure the API is working on a simple event get

--- events.rb
+++ events.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'eventful/api'
+require 'json'
 
 # This module houses the different hooks into events and where the information
 # is queried from.
@@ -7,10 +8,18 @@ module Events
 
     # A wrapper class that controls calling the EventFul API
     class EventfulWrapper
+
         def initialize()
             #Start our eventful API session
             @eventful = Eventful::API.new 'gRD2ZtJwjqKVNht3'
         end
+
+        def event_by_id(id)
+            results = @eventful.call 'events/get',
+                                    :id => id
+           results.to_json
+        end
+
     end
 
     class EventBriteWrapper
--- tests.rb
+++ tests.rb
@@ -6,5 +6,9 @@ class TestEvents < Test::Unit::TestCase
         eventful = Events::EventfulWrapper.new()
         assert_not_nil(eventful)
     end
-
+    def test_event_query
+        eventful = Events::EventfulWrapper.new()
+        json_result = JSON.parse(eventful.event_by_id('E0-001-000278174-6'))
+        assert_equal "Ohio", json_result['region']
+    end
 end
