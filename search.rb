d4acf89da406c03c763c5268659b07d390972e4c
Erik Johnson <erjohnson@blackberry.com>
2013-04-28 19:31:22 -0400

    Adding the search layer holder file

---
+++ search.rb
@@ -0,0 +1,20 @@
+require 'rubygems'
+require 'json'
+require 'eventful/api'
+
+module Evently
+
+    # Search module that translates the categories request into the actual event API searches
+    # A wrapper class that controls calling the EventFul API
+    class Search
+
+        def initialize()
+            raise 'Not yet implemented'
+        end
+
+        def get_categories()
+            raise 'Not yet implemented'
+        end
+    end
+
+end
